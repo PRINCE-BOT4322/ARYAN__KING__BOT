712482ad3b2e2f76c2959e8e4bb821717a454b50
PRINCE-BOT4322 <ik1673254@gmail.com>
2024-10-31 18:02:17 +0530

    Update 0_VIRAT-ARA.js

--- modules/commands/0_VIRAT-ARA.js
+++ modules/commands/0_VIRAT-ARA.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "araara",
+  name: "welcome ho gaya meri jaan ummah ummah kr ke than than kar ke",
     version: "2.1.1",
   hasPermssion: 0,
   credits: "PRINCE RAJPUT", 
@@ -13,15 +13,15 @@ module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils
   var name = await Users.getNameUser(event.senderID);
   var { threadID, messageID } = event;
   let react = event.body.toLowerCase();
-  if(react.includes("🙄") ||
-     react.includes("😋") ||
-     react.includes("🥵🥵") ||
+  if(react.includes("welcome") ||
+     react.includes("WELCOME") ||
+     react.includes("Wlcm") ||
 react.includes("ara")) {
     var msg = {
-        body: `🙄ara~ara🙄`,attachment: fs.createReadStream(__dirname + `/noprefix/ara.mp3`)
+        body: `welcome hogaya na meri jaan Ummah ummah kr ke than than kr ke`,attachment: fs.createReadStream(__dirname + `/noprefix/ara.mp3`)
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🤫", event.messageID, (err) => {}, true)
+    api.setMessageReaction("💋", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
